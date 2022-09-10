3cf3c5b2fa9e724aba7a951540eef08b2084e1d7
AdelhamA <adell97.work@gmail.com>
2022-09-10 06:05:06 +0200

    add : svgs icon

--- src/svgs/index.ts
+++ src/svgs/index.ts
@@ -1,2 +1,32 @@
 import { ReactComponent as Logo } from "./logo.svg";
-export { Logo };
+import { ReactComponent as Hamburger } from "./hamburger.svg";
+import { ReactComponent as Search } from "./search.svg";
+import { ReactComponent as Linkdin } from "./linkdin.svg";
+import { ReactComponent as Youtube } from "./youtube.svg";
+import { ReactComponent as Insta } from "./insta.svg";
+
+import { ReactComponent as Hammer } from "./hammer.svg";
+import { ReactComponent as Vikings } from "./vikings.svg";
+import { ReactComponent as Mascus } from "./mascus.svg";
+import { ReactComponent as Lemsip } from "./lem.svg";
+import { ReactComponent as Points } from "./points.svg";
+
+import { ReactComponent as Achive } from "./achive.svg";
+import { ReactComponent as Tackled } from "./tackeld.svg";
+import { ReactComponent as Speach } from "./speach.svg";
+export {
+  Speach,
+  Tackled,
+  Achive,
+  Logo,
+  Hamburger,
+  Points,
+  Search,
+  Insta,
+  Youtube,
+  Linkdin,
+  Lemsip,
+  Vikings,
+  Mascus,
+  Hammer,
+};
