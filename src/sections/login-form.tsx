21673e98430bf5174288ad05efdd708104524dbe
AdelhamA <adell97.work@gmail.com>
2022-09-10 06:37:17 +0200

    fix container

--- src/sections/login-form.tsx
+++ src/sections/login-form.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import { Container } from "react-bootstrap";
 import { Logo } from "../svgs";
 import { FormLogin } from "../components/form-login";
 import styles from "./form.module.css";
 import { LOGINTEXT } from "../constants/index";
 export const LoginFormSection = () => {
   return (
-    <section style={{ padding: 60, display: "flex", flexDirection: "column" }}>
+    <section className={styles.Container}>
       <Logo />
       <text className={styles.TextForm}>
         <span>Log in</span> {LOGINTEXT}
