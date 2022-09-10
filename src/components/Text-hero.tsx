6d2059f6a26b7f930c5602cc10f360cfc47a8d3c
AdelhamA <adell97.work@gmail.com>
2022-09-10 06:09:58 +0200

    feat: text-hero component

---
+++ src/components/Text-hero.tsx
@@ -0,0 +1,25 @@
+import { HERO_Section } from "../constants/index";
+import { Button } from "./button";
+import "./header.css";
+export const TextHero = () => {
+  return (
+    <>
+      <p className="fText">Listen, Learn, Guide</p>
+      <p className="sText">
+        Realise your goals {"\n"}
+        <span>And live </span>as you wish
+      </p>
+      <p className="tText">{HERO_Section}</p>
+      <Button
+        border={"none"}
+        color={"#CF0303"}
+        type="button"
+        height={"66px"}
+        children={<text className="buttontxt">Find out who they are</text>}
+        radius={"50px"}
+        width={"226px"}
+        className={"button"}
+      />
+    </>
+  );
+};
